52f11ee5caeb8bbb405f5149e6b6d5d29c9411cb
G Jake Gebbie <ggebbie@whoi.edu>
2024-06-27 12:55:43 -0400

    help ext compile

--- ext/UnitfulLinearAlgebraExt.jl
+++ ext/UnitfulLinearAlgebraExt.jl
@@ -1,21 +1,21 @@
 module UnitfulLinearAlgebraExt
 
-using MultipliableDimArrays, UnitfulLinearAlgebra
+using MultipliableDimArrays, UnitfulLinearAlgebra, LinearAlgebra
 
 # Write your package code here.
-function LinearAlgebra.diag(A::AbstractUnitfulMatrix{T}) where T <: AbstractDimArray
+# function LinearAlgebra.diag(A::AbstractUnitfulMatrix{T}) where T <: AbstractDimArray
 
-    if uniform(A)
-        inside_type = typeof(getindexqty(A,1,1))
-        sigma = Array{inside_type}(undef,size(A))
-    else
-        sigma = Array{Quantity}(undef,size(A))
-    end
+#     if uniform(A)
+#         inside_type = typeof(getindexqty(A,1,1))
+#         sigma = Array{inside_type}(undef,size(A))
+#     else
+#         sigma = Array{Quantity}(undef,size(A))
+#     end
     
-    for i in eachindex(A)
-        sigma[i] = getindexqty(A,i,i)
-    end
-    return DimArray(sigma,dims(A))
-end
+#     for i in eachindex(A)
+#         sigma[i] = getindexqty(A,i,i)
+#     end
+#     return DimArray(sigma,dims(A))
+# end
 
 end
