43a8fdfec654da07d688531598230b9f1f5456d0
Documenter.jl <documenter@juliadocs.github.io>
2024-06-27 14:35:48 +0000

    build based on 84a79d6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultipliableDimArrays","category":"page"},{"location":"#MultipliableDimArrays","page":"Home","title":"MultipliableDimArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MultipliableDimArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultipliableDimArrays]","category":"page"}]
+}
